f09ff09cffc91dcb23f9807dce13b693ec200f69
SHYAM KAARTHIKEYEN B <113837941+shyamkaarthikeyan@users.noreply.github.com>
2025-04-29 13:18:04 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
-# Install Tesseract OCR
-apt-get update && apt-get install -y tesseract-ocr
+# Update package list and install Tesseract OCR
+apt-get update
+apt-get install -y tesseract-ocr
 
 # Install Python dependencies
 pip install -r requirements.txt
